d833a12fc9a721420da3a79d88b03675b994df3c
leonidspiridonov <2003.leonid2003@gmail.com>
2026-02-22 23:48:05 +0300

    chore: fix second migration

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -2,28 +2,22 @@ ALTER TABLE product
     ADD COLUMN IF NOT EXISTS price DOUBLE PRECISION;
 
 UPDATE product p
-SET price = pi.price FROM product_info pi
-WHERE p.id = pi.product_id;
+SET price = pi.price FROM   product_info pi
+WHERE pi.id = p.id;
 
 ALTER TABLE orders
     ADD COLUMN IF NOT EXISTS date_created DATE;
 
 UPDATE orders o
-SET date_created = od.date_created,
-    status       = COALESCE(o.status, od.status) FROM orders_date od
-WHERE o.id = od.order_id;
-
-ALTER TABLE product
-    ADD PRIMARY KEY (id);
-
-ALTER TABLE orders
-    ADD PRIMARY KEY (id);
+SET date_created = od.date_created FROM   orders_date od
+WHERE od.id = o.id;
 
 ALTER TABLE order_product
-    ADD CONSTRAINT fk_order_product_order
-        FOREIGN KEY (order_id) REFERENCES orders (id),
-    ADD CONSTRAINT fk_order_product_product
-        FOREIGN KEY (product_id) REFERENCES product(id);
+    ADD COLUMN IF NOT EXISTS quantity INTEGER;
 
-DROP TABLE product_info;
-DROP TABLE orders_date;
+ALTER TABLE order_product
+    ADD CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders (id),
+    ADD CONSTRAINT fk_product FOREIGN KEY (product_id) REFERENCES product(id);
+
+DROP TABLE IF EXISTS product_info;
+DROP TABLE IF EXISTS orders_date;
